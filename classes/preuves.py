086221caa73d57b677889b41c63e47fdd2457413
Salfahin <quentinhenrard0@gmail.com>
2024-11-12 15:29:03 +0100

    Add files via upload

--- classes/preuves.py
+++ classes/preuves.py
@@ -1,7 +1,8 @@
 from datetime import date, datetime
 class Preuve:
 
-    def __init__(self, id: int, nom : str, enquete_liee:int, type = "non spécifié", lieu = "non spécifié", date_preuve = date.today(), heure = "non spécifée"):
+    def __init__(self, id: int , nom : str,enquete_liee:int,  type = " Type non spécifié", lieu = "Lieu non spécifié",
+                 date_preuve = date.today(), heure = "Heure non spécifée"):
         self.id = id
         self.nom = nom
         self.enquete_liee = enquete_liee
@@ -9,7 +10,4 @@ class Preuve:
         self.lieu = lieu
         self.date_preuve = date_preuve
         self.heure = heure
-
-    def __str__(self):
-        return f"{self.id} - {self.nom}"
 
