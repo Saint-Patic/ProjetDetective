71e2d45dbc8ccd84cd83ad51d1a1749c4090d8a4
Nathan <153100527+Akanaifu@users.noreply.github.com>
2024-11-12 15:16:10 +0100

    ajout de try except

--- classes/person.py
+++ classes/person.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 
+from datetime import datetime
+
+
 class Person:
     def __init__(
         self,
@@ -37,61 +40,22 @@ class Person:
 
     @property
     def date_de_deces(self):
-        return self.date_de_deces.strftime("%Y-%m-%d")
+        return self._date_de_deces.strftime("%Y-%m-%d")
 
     @date_de_deces.setter
     def date_de_deces(self, value: str) -> None:
         try:
             date_obj = datetime.strptime(value, "%Y-%m-%d")
+            if date_obj > datetime.now():
+                print("Erreur : La date de décès ne peut pas être dans le futur.")
+            elif date_obj < self._date_de_naissance:
+                print(
+                    "Erreur : La date de décès doit être supérieure à la date de naissance."
+                )
+            else:
+                self._date_de_deces = date_obj
         except ValueError:
-            raise ValueError("Le format de la date doit être AAAA-MM-JJ.")
-
-        if date_obj > datetime.now():
-            raise ValueError("La date de décès ne peut pas être dans le futur.")
-
-        if date_obj < self.date_de_naissance:
-            raise ValueError(
-                "La date de décès doit être supérieure à la date de naissance."
-            )
-
-        self.date_de_deces = date_obj
-
-    def add_interrogatoire(self, date: str, enqueteur, num_enquete: int) -> None:
-        interrogatoires_date = self.interrogatoires.get(date, [])
-        date_modifiee = datetime.strptime(date, "%Y-%m-%d")
-        if (
-            enqueteur._date_de_naissance > date_modifiee
-            or self._date_de_naissance > date_modifiee
-        ):
-            raise ValueError("L'enquêteur ou l'interrogé n'est pas encore né")
-
-        if (
-            enqueteur._date_de_deces < date_modifiee
-            or self._date_de_deces < date_modifiee
-        ):
-            raise ValueError(
-                "L'enquêteur ou l'interrogé est mort. Il ne peut pas participer à l'interrogatoire"
-            )
-
-        interrogatoires_date.append(
-            {"enqueteur": enqueteur.prenom, "num_enquete": num_enquete}
-        )
-        self.interrogatoires[date] = interrogatoires_date
-
-    def get_interrogatoires(self, date: str) -> list:
-
-        try:
-            datetime.strptime(date, "%Y-%m-%d")
-        except ValueError:
-            raise ValueError("Le format de la date doit être AAAA-MM-JJ.")
-
-        if date not in self.interrogatoires:
-            raise KeyError(f"Aucun interrogatoire trouvé pour la date : {date}")
-
-        return self.interrogatoires[date]
-
-    def __str__(self) -> str:
-        return f"{self.nom} {self.prenom}, né le {self.date_de_naissance}, travaille comme {self.metier}"
+            print("Erreur : Le format de la date doit être AAAA-MM-JJ.")
 
 
 if __name__ == "__main__":
