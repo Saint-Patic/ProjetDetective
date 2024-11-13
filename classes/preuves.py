33ea744d3e8e15bd6c754d8ccd6237af5e96d90e
Salfahin <quentinhenrard0@gmail.com>
2024-11-13 11:57:54 +0100

    Ajout d'un __str__

--- classes/preuves.py
+++ classes/preuves.py
@@ -1,8 +1,18 @@
 from datetime import date, datetime
+
+
 class Preuve:
 
-    def __init__(self, id: int , nom : str,enquete_liee:int,  type = " Type non spécifié", lieu = "Lieu non spécifié",
-                 date_preuve = date.today(), heure = "Heure non spécifée"):
+    def __init__(
+        self,
+        id: int,
+        nom: str,
+        enquete_liee: int,
+        type=" Type non spécifié",
+        lieu="Lieu non spécifié",
+        date_preuve=date.today(),
+        heure="Heure non spécifée",
+    ):
         self.id = id
         self.nom = nom
         self.enquete_liee = enquete_liee
@@ -11,3 +21,10 @@ class Preuve:
         self.date_preuve = date_preuve
         self.heure = heure
 
+    def __str__(self) -> str:
+        return (f"ID: {self.id}, Nom: {self.nom}, Enquete liée: {self.enquete_liee}, Type: {self.type},"
+                f" Lieu: {self.lieu}, "f" Date: {self.date_preuve}, Heure: {self.heure}")
+
+
+if __name__ == "__main__":
+    pass
