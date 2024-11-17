bf0abe48e5d49d36f5b8348ccd1c4c29f52f5c86
Salfahin <quentinhenrard0@gmail.com>
2024-11-18 00:59:46 +0100

    Ajout de docstrings

--- classes/event.py
+++ classes/event.py
@@ -4,19 +4,36 @@ from datetime import date
 class Evenement:
 
     def __init__(self, id: int, nom: str, enquete_liee : int , date_event=date.today(), lieu = "Lieu pas précisé"):
+        """
+        Pré : id (int), nom (str), enquete_liee (int), lieu (str) (optionel) ,
+        date_de_naissance (str) au format "YYYY-MM-DD"
+        Post : Crée une instance de la classe Evenement avec les attributs spécifiés
+        """
         self.id = id
         self.nom = nom
         self.enquete_liee = enquete_liee
         self.date_event = date_event
         self.lieu = lieu
 
-    def ajouter_date(self, nouvelle_date)->None:
+    def ajouter_date(self, nouvelle_date):
+        """
+        Pré : date au format YYYY-MM-DD
+        Post : Définit la date ou l'évenement s'est passé
+        """
         self.date_event = nouvelle_date
 
-    def add_lieu(self, lieu) -> None:
+    def ajouter_lieu(self, lieu):
+        """
+        Pré : lieu (str)
+        Post : Définit l'attribut lieu de l'évenement
+        """
         self.lieu = lieu
 
     def __str__(self) -> str:
+        """
+        Pré : -
+        Post : return une string contenant les attributs
+        """
         return (f"ID: {self.id}, Nom: {self.nom}, Enquete liée: {self.enquete_liee}, Date: {self.date_event},"
                 f"Lieu: {self.lieu}")
 
