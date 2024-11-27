50f41a33bb1d1c844caa43c681322b9920ebd20f
Nathan <153100527+Akanaifu@users.noreply.github.com>
2024-11-27 13:14:22 +0100

    ajout test enquete

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Enquete:
         self,
         nom: str,
         date_de_debut: str,
-        date_de_fin = "Enquête non clôturer",
+        date_de_fin="Enquête non clôturer",
         liste_preuves=[],
         personne_impliquee=[],
     ) -> None:
@@ -260,6 +260,7 @@ if __name__ == "__main__":
     Fraude = creer_enquete("Fraude", "2023-07-01", "2023-12-31")
     Meurtre = Enquete("Meurtre", "2003-08-04", "2005-02-26", [], [])
     Cambriolage = Enquete("Cambriolage", "2010-06-15", "2011-08-01", [], [])
+
     Alexis = Personne("Demarcq", "Alexis", "2003-08-04", "Homme")
     Nathan = Employe("Lemaire", "Nathan", "2003-01-01", "Homme")
     Quentin = Suspect("Henrard", "Quentin", "2003-08-04", "Homme")
@@ -279,6 +280,9 @@ if __name__ == "__main__":
 
     # Sauvegarder les enquêtes
     Vol.sauvegarder_enquete()
+    Fraude.sauvegarder_enquete()
+    Meurtre.sauvegarder_enquete()
+    Cambriolage.sauvegarder_enquete()
 
     # # Afficher les preuves
     # Meurtre.add_preuves("Arme")
