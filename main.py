0004e51c3191b2676de832b4b27c7c64bcb0a528
Nathan <153100527+Akanaifu@users.noreply.github.com>
2024-12-14 16:17:57 +0100

    ajout interface ligne de commandes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utilitaire import utils
+import json
 from classes import (
     Personne,
     Temoin,
@@ -23,61 +24,36 @@ def creer_enquete(nom, date_de_debut, date_de_fin):
     return nouvelle_enquete
 
 
+def afficher_enquete():
+    with open("fichiers/enquetes.json", "r", encoding="utf-8") as f:
+        enquetes = json.load(f)
+        for enquete in enquetes:
+            print(
+                f"{enquete['nom']} ({enquete['date_de_debut']} - {enquete['date_de_fin']})"
+            )
+
+
 if __name__ == "__main__":
+    sortie = False
+    while not sortie:
+        print("\nMenu:")
+        print("1. Créer une nouvelle enquête")
+        print("2. Choisir une enquete")
+        print("3. Afficher les enquêtes existantes")
+        print("4. Quitter")
+        choix = input("Votre choix: ")
+        if choix == "1":
+            nom = input("Nom de l'enquête: ")
+            date_de_debut = input("Date de début (YYYY-MM-DD): ")
+            date_de_fin = input("Date de fin (YYYY-MM-DD): ")
+            nouvelle_enquete = creer_enquete(nom, date_de_debut, date_de_fin)
+            print(f"Enquête créée: {nouvelle_enquete}")
+        elif choix == "2":
+            afficher_enquete()
+        elif choix == "3":
+            pass
+        elif choix == "4":
+            sortie = True
 
-    Vol = creer_enquete("Vol", "2023-01-01", "2023-06-30")
-    Fraude = creer_enquete("Fraude", "2023-07-01", "2023-12-31")
-    Meurtre = Enquete("Meurtre", "2003-08-04", "2005-02-26", [], [])
-    Cambriolage = Enquete("Cambriolage", "2010-06-15", "2011-08-01", [], [])
-
-    Alexis = Personne("Demarcq", "Alexis", "2003-08-04", "Homme")
-    Nathan = Employe("Lemaire", "Nathan", "2003-01-01", "Homme")
-    Quentin = Suspect("Henrard", "Quentin", "2003-08-04", "Homme")
-    Tristan = Criminel("Valcke", "Tristan", "2003-08-04", "Homme")
-
-    Meurtre.ajouter_personne(Alexis)
-    Meurtre.ajouter_personne(Quentin)
-    Cambriolage.ajouter_personne(Alexis)
-    Cambriolage.ajouter_personne(Nathan)
-
-    # Afficher les enquêtes liées
-    Vol.ajouter_enquetes_liees(Fraude)
-    Meurtre.afficher_enquetes_liees()
-    Meurtre.ajouter_enquetes_liees(Cambriolage)
-    Meurtre.afficher_enquetes_liees()
-    Vol.afficher_enquetes_liees()
-
-    # Sauvegarder les enquêtes
-    # Vol.sauvegarder_enquete()
-    # Fraude.sauvegarder_enquete()
-    # Meurtre.sauvegarder_enquete()
-    # Cambriolage.sauvegarder_enquete()
-
-    # Afficher les preuves
-    # Meurtre.ajouter_preuves("Arme")
-    # Meurtre.ajouter_preuves("Indice")
-    # Cambriolage.ajouter_preuves("Arme")
-    # Meurtre.afficher_preuves()
-    # Cambriolage.afficher_preuves()
-
-    # Afficher les enquêtes existantes
-    # Enquete.afficher_enquetes()
-
-    # Afficher les évènements
-    # Meurtre.ajouter_evenement("Découverte du corps")
-    # Meurtre.afficher_evenements()
-    # Cambriolage.afficher_evenements()
-
-    # Afficher les interrogatoires
-    # Alexis.ajouter_interrogatoire("2004-01-01", Nathan, Meurtre.id)
-    # Quentin.ajouter_interrogatoire("2005-11-22", Nathan, Cambriolage.id)
-    # Alexis.ajouter_interrogatoire("2002-01-21", Nathan, Cambriolage.id)
-    # Meurtre.afficher_interrogatoires(Meurtre.id)
-    # Cambriolage.afficher_interrogatoires(Cambriolage.id)
-
-    # Clôturer une enquête
-    # Cambriolage.cloturer_enquete()
-
-    # Générer un rapport
-    # Enquete.generer_rapport(1)
-    # Enquete.generer_rapport(2)
+        rep = input("Quitter (oui/non) ?")
+        sortie = True if rep == "oui" else False
